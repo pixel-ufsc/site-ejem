ae5d6859d1879a595cd7d4ef109154cc31e1e095
unknown <livia.ferrao16@hotmail.com>
2022-12-29 23:44:26 -0300

    slider

--- frontend/components/Home/OurFeedBackComponent/index.js
+++ frontend/components/Home/OurFeedBackComponent/index.js
@@ -1,28 +1,65 @@
 import styles from "./ourfeedback.module.css";
+import FeedBackCard from "./FeedBackCard"
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import React, { Component, useState, useEffect } from "react";
+import Slider from "react-slick";
+
 // import Image from 'next/image'
 // import email from '../../../public/home/contact/email.png'
 
 
-export default function OurFeedBackComponent() {
-    return(
+export default class OurFeedBackComponent extends Component {
+    // pegar imagem e descrição 
+
+    render() {
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+      return (
         <section>
             <section className={styles.section1}>
                 <div className={styles.title}>
-                    Feedback de clientes
+                    Feedback de clientes    
                 </div>
             </section>
-            <section className={styles.section2}>
-                <div className={styles.imageContainer}>
-                    <div className={styles.image}>imagem</div>
+            <Slider {...settings}>
+                <div>
+                    <FeedBackCard></FeedBackCard>
                 </div>
-                <div className={styles.textContainer}>
-                    <div className={styles.name}>Titulo</div>
-                    <div className={styles.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt viverra volutpat nec turpis viverra. Neque, dolor, non aliquam egestas. Ut vulputate sagittis amet, adipiscing est in. Ornare arcu tempus nunc at aliquet adipiscing quis sed. A donec risus lorem leo scelerisque lorem eget. Pretium integer aliquam bibendum tellus viverra eget in. Iaculis tincidunt nunc, a vitae, integer et massa.
-                    </div>
+                <div>
+                    <FeedBackCard></FeedBackCard>
                 </div>
-            </section>
+                <div>
+                    <FeedBackCard></FeedBackCard>
+                </div>
+            </Slider>
             <section className={styles.section3}/>
         </section>
-    )
-}
+      );
+    }
+  }
+
+
+// export default function OurFeedBackComponent() {
+//     return(
+//         <section>
+//             <section className={styles.section1}>
+//                 <div className={styles.title}>
+//                     Feedback de clientes    
+//                 </div>
+//             </section>
+//             {/* passar imagem e descrição */}
+//             <FeedBackCard />
+            
+//             <section className={styles.section3}/>
+//         </section>
+//         )
+// }
+
