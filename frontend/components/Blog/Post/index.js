8e00bfc9f58feb6fe6bbecae95cd167f73bdca7d
Matheus Antonio <17206723+hknmtt@users.noreply.github.com>
2023-01-17 16:09:52 -0300

    Conectado backend

---
+++ frontend/components/Blog/Post/index.js
@@ -0,0 +1,26 @@
+import styles from "./post.module.css";
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function PostComponent({ image, htmlString, data }) {
+    return(
+        <section className={styles.section}>
+            <div className={styles.container}>
+                <Image src={image} alt="imagem" width={1000} height={500} />
+                <div className={styles.titulo}> {data.titulo}</div>
+                <div className={styles.subtitulo}> {data.subtitulo} </div>
+                <div className={styles.autor}> {data.autor.data.attributes.nome} </div>
+                <div className={styles.data}> {data.data_publicacao} </div>
+
+                    {data.categorias.data.map((categoria) => { return (
+                    <div className={styles.categoria}>
+                        {categoria.attributes.tag}
+                    </div>
+                    )})}
+                <div className={styles.conteudo} dangerouslySetInnerHTML={{ __html: htmlString }} />
+
+            </div>
+        </section>
+    )
+}
---
+++ frontend/pages/blog/[slug].js
@@ -0,0 +1,58 @@
+import React from "react";
+import fm from "front-matter";
+import Head from "next/head";
+import { marked } from "marked";
+import NavigationBar from "../../components/Shared/NavigationBar";
+import PostComponent from "../../components/Blog/Post";
+import OurContactComponent from "../../components/Shared/OurContactComponent";
+import FooterComponent from "../../components/Shared/FooterComponent";
+
+export const getStaticPaths = async () => {
+    let result = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/posts`);
+    result = await result.json()
+    return {
+    paths: result.data.map(result => ({
+        params: {slug: result.attributes.slug},
+    })),
+    fallback: false,
+    };
+};
+
+export const getStaticProps = async ({ params }) => {
+    const res = await fetch(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/posts?filters[slug][$eq]=${params.slug}&populate=%2A`
+    );
+    //console.log(params);
+    //console.log(`${process.env.NEXT_PUBLIC_STRAPI_URL}/posts?filters[slug][$eq]=${params.slug}&populate=imagem&populate=autor`)
+    const markdownWithMeta = await res.json();
+    const parsedMarkdown = fm(markdownWithMeta.data[0].attributes.conteudo);
+    const htmlString = marked(parsedMarkdown.body);
+    const image = markdownWithMeta.data[0].attributes.imagem.data.attributes.formats.medium.url;
+    return {
+    props: {
+        image,
+        htmlString,
+        data: markdownWithMeta.data[0].attributes,
+    },
+    };
+};
+
+export default function Post({ image, htmlString, data }) {
+    return (
+    <>
+        <Head>
+        <title>{data.titulo}</title>
+            <meta name="description" content={data.titulo} />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+            <NavigationBar />
+            <PostComponent image={image} htmlString={htmlString} data={data} />
+            <OurContactComponent />
+            <FooterComponent />
+
+        </main>
+        
+    </>
+    );
+}
