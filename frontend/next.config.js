16fce36dfaf8a82a5d246dbe060bfe6b7f2ae034
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-02-13 21:09:37 -0300

    Adicionado domínio aceito para as imagens

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-      images: {
-        domains: [`${process.env.DOMAIN}`]
-      },
-}
-
-module.exports = nextConfig
+  images: {
+    domains: [`${process.env.DOMAIN}`, "res.cloudinary.com"]
+  },
+};
+module.exports = nextConfig;
