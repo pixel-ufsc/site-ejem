15a8f752a3a2473c8787d11d2ee18deddabf24dd
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-02-16 11:31:13 -0300

    Corrigida responsividade do contato e ajustado estilos do feedback

--- frontend/components/QuemSomos/OurPartnersComponent/index.js
+++ frontend/components/QuemSomos/OurPartnersComponent/index.js
@@ -8,19 +8,19 @@ import LabMat from '../../../public/quemSomos/partners/labmat.png'
 export default function OurPartnersComponent() {
     return(
         <section>
-            <section className={styles.section1}>
+            <section className={styles.firstPartner}>
                 <div className={styles.title}>Parceria com o Labmat</div>
                 <div className={styles.textContainer}>
                     <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor elit amet viverra nunc commodo. Auctor sed dictum porta sed cursus ut. Sit elementum aliquam elit sapien quis dolor feugiat placerat. Feugiat tempus blandit dictumst bibendum accumsan habitasse eu id etiam. </div>
                     <Image className={styles.image1} src={LabMat} alt="Labmat"/>
                 </div>
             </section>
-            <section className={styles.section}>
+            <section className={styles.otherPartners}>
                 <div className={styles.title}>Parcerias</div>
                 <div className={styles.images}>
-                        <Image className={styles.image} src={Ampera} alt="Ampera"/>
-                        <Image className={styles.image} src={LabSolda} alt="LabSolda"/>
-                        <Image className={styles.image} src={Empower} alt="Empower"/>
+                    <Image className={styles.image} src={Ampera} alt="Ampera"/>
+                    <Image className={styles.image} src={LabSolda} alt="LabSolda"/>
+                    <Image className={styles.image} src={Empower} alt="Empower"/>
                 </div>
             </section>
         </section>
