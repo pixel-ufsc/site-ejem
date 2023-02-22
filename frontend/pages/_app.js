3dfa8727fa5b05b9e41c7dfb7efa62366b6efc11
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-02-22 15:43:58 -0300

    Adicionada barra de progresso de carregamento das páginas

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <NextNProgress color={'#79ABDE'} height={2} />
+            <Component {...pageProps} />
+        </>
+    );
 }
