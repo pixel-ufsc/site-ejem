253c905d737baff5a18dd922e8c7a6170aea00db
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-02-22 16:25:07 -0300

    Reformatado ícone do 'Whatsapp'

--- frontend/components/Shared/WppComponent/index.js
+++ frontend/components/Shared/WppComponent/index.js
@@ -2,14 +2,12 @@ import styles from './wpp.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import wpp_icon from '../../../public/home/cards_solutions/wpp_icon.png';
+import { WhatsappIcon } from '../../../public/icons/icons';
 
 export default function WppComponent() {
     return (
-        <>
-            <Link className={styles.icon} href="/">
-                <Image src={wpp_icon} alt="wpp" />
-            </Link>
-        </>
+        <Link className={styles.icon} href="/">
+            <WhatsappIcon />
+        </Link>
     );
 }
