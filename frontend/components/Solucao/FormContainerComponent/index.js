e2c3ec7090e199efaa3cce1a67fc189bce54bca8
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-02-23 13:36:36 -0300

    Criado componente de 'FormContainer'

---
+++ frontend/components/Solucao/FormContainerComponent/index.js
@@ -0,0 +1,10 @@
+import styles from './formcontainer.module.css';
+import OurFormComponent from '../../Shared/OurFormComponent';
+
+export default function FormContainerComponent() {
+    return (
+        <div className={styles.container}>
+            <OurFormComponent />
+        </div>
+    );
+}
