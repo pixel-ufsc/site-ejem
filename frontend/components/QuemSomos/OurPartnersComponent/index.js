45d3b2b5152fd1489e7bae52a4cd488133280689
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-02-23 21:33:43 -0300

    Subsituídos textos placeholders

--- frontend/components/QuemSomos/OurPartnersComponent/index.js
+++ frontend/components/QuemSomos/OurPartnersComponent/index.js
@@ -10,7 +10,7 @@ export default function OurPartnersComponent() {
         <section>
             <MainPartner
                 title={'Parceria com o Labmat'}
-                text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor elit amet viverra nunc commodo. Auctor sed dictum porta sed cursus ut. Sit elementum aliquam elit sapien quis dolor feugiat placerat. Feugiat tempus blandit dictumst bibendum accumsan habitasse eu id etiam.`}
+                text={`Avaliado como um dos maiores laboratórios de materiais da América Latina por seus serviços de excelência, desenvolvimento de materiais e processos inovadores,      o LABMAT é parceiro da EJEM, desde a criação de nossa EJ, fornecendo serviços como caracterização de materiais, analise de falhas, ensaios mecânicos entre outros serviços   Além disso, contamos com o suporte fornecido por professores e doutores em engenharia que atuam no laboratório trazendo soluções praticas e inovadoras para nossos serviços.`}
                 imageUrl={LabMat}
                 alt={'Labmat'}
             />
