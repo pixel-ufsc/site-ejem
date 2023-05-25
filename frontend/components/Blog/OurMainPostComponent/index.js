9f1ab2e7176f85e4e49302f5e06c672b8c44cca1
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-05-25 16:17:52 -0300

    Corrigida altura do post principal do blog

--- frontend/components/Blog/OurMainPostComponent/index.js
+++ frontend/components/Blog/OurMainPostComponent/index.js
@@ -9,7 +9,6 @@ export default function OurMainPostComponent({ mainPost }) {
     const postTags = post?.categorias?.data;
     const slug = post?.slug ? 'blog/' + post?.slug : '';
 
-
     const convertDate = (date) => {
         // Convert 2022-12-29 to 29/12/2022
         const dateArray = date.split('-');
@@ -25,27 +24,44 @@ export default function OurMainPostComponent({ mainPost }) {
         router.push(slug);
     };
 
-
     return (
         <section className={styles.container} onClick={handleClick}>
             <div className={styles.contentWrapper}>
                 <div className={styles.textContainer}>
-                    <h2>{post.titulo}</h2>
-                    <h4>{post.subtitulo}</h4>
-                    <p>{post.resumo}</p>
-                    <p>{convertDate(post.data_publicacao)}</p>
-                    <div className={styles.tagsContainer}>
-                        {postTags.map((tag) => (
-                            <PostTagComponent text={tag?.attributes?.tag} key={tag?.id} />
-                        ))}
+                    <div className={styles.text}>
+                        <h2>{post.titulo}</h2>
+                        <h4>{post.subtitulo}</h4>
+                        <p>{post.resumo}</p>
+                    </div>
+                    <div className={styles.textInfo}>
+                        <p className={styles.date}>{convertDate(post.data_publicacao)}</p>
+                        <div className={styles.tagsContainer}>
+                            {postTags.map((tag) => (
+                                <PostTagComponent text={tag?.attributes?.tag} key={tag?.id} />
+                            ))}
+                        </div>
                     </div>
                 </div>
 
-                <img
-                    className={styles.image}
-                    src={postImageFormats?.large?.url ? postImageFormats?.large?.url : postImageFormats?.medium?.url}
-                    alt="Imagem do post em destaque"
-                />
+                <div className={styles.imageContainer}>
+                    <img
+                        fill
+                        src={
+                            postImageFormats?.large?.url ? postImageFormats?.large?.url : postImageFormats?.medium?.url
+                        }
+                        alt="Imagem do post em destaque"
+                    />
+                </div>
+
+                {/*                 <div className={styles.imageContainer}>
+                    <img
+                        className={styles.image}
+                        src={
+                            postImageFormats?.large?.url ? postImageFormats?.large?.url : postImageFormats?.medium?.url
+                        }
+                        alt="Imagem do post em destaque"
+                    />
+                </div> */}
             </div>
         </section>
     );
--- frontend/components/Blog/PostTagComponent/index.js
+++ frontend/components/Blog/PostTagComponent/index.js
@@ -1,5 +1,5 @@
 import styles from './posttag.module.css';
 
 export default function PostTagComponent({ text = 'Teste' }) {
-    return <h4 className={styles.tag}>{text}</h4>;
+    return <p className={styles.tag}>{text}</p>;
 }
