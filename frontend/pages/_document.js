84ae7f1b74417932eb1529ad8fe917fc2bcc564a
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-06-20 23:55:57 -0300

    Adicionado Script 'Google Tag Manager' para o Google Analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                {/* Google Tag Manager */}
+                <Script
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TP2JS9W');`,
+                    }}
+                ></Script>
+                {/* End Google Tag Manager */}
+            </Head>
             <body>
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TP2JS9W" height="0" width="0" style="display:none;visibility:hidden">`,
+                    }}
+                ></noscript>
                 <Main />
                 <NextScript />
             </body>
